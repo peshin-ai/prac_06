bfd0e0700887a746d8da0c05035ca807577d7a3f
hoangtuanvp2011 <t00aienl@gmail.com>
2020-12-22 18:21:12 +0700

    finish the practice at file car.py and used_cars.py

--- used_cars.py
+++ used_cars.py
@@ -6,14 +6,23 @@ from car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
-    my_car.drive(30)
-    print("fuel =", my_car.fuel)
-    print("odo =", my_car.odometer)
-    print(my_car)
+    # my_car = Car(180)
+    # my_car.drive(30)
+    # print("fuel =", my_car.fuel)
+    # print("odo =", my_car.odometer)
+    # print(my_car)
+    #
+    # print("Car {}, {}".format(my_car.fuel, my_car.odometer))
+    # print("Car {self.fuel}, {self.odometer}".format(self=my_car))
 
-    print("Car {}, {}".format(my_car.fuel, my_car.odometer))
-    print("Car {self.fuel}, {self.odometer}".format(self=my_car))
+    limo = Car("limo", 100)
+    # print('{}'.format(limo.name))
+    limo.add_fuel(20)
+    print("the amount of fuel in the car is {}".format(limo.fuel))
+    limo.drive(115)
+    print(" the car's odometer reading is {}".format(limo.odometer))
+    limo.__str__()
 
 
-main()
+if __name__ == '__main__':
+    main()
