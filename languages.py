8b13b833e8710da873a6e5db05a36f16d01580a0
hoangtuanvp2011 <t00aienl@gmail.com>
2020-12-23 00:11:47 +0700

    complete the languages.py and class programming_language.py

--- languages.py
+++ languages.py
@@ -1,8 +1,19 @@
-from programming_language import Programming_Language
+from programming_language import ProgrammingLanguage
 
 def main():
-    ruby = Programming_Language("Ruby", "Dynamic", True, 1995)
-    python = Programming_Language("Python", "Dynamic", True, 1991)
-    visual_basic = Programming_Language("Visual Basic", "Static", False, 1991)
+    """ create 3 object base on class ProgrammingLanguage """
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    """ create the array to store 3 object """
+    array = [ruby, python, visual_basic]
+    print("The dynamically typed languages are:")
+
+    """ print value if the language is dynamic"""
+    for item in array:
+        if item.is_dynamic():
+            print(item.language)
+
 if __name__ == '__main__':
     main()
--- programming_language.py
+++ programming_language.py
@@ -1,8 +1,20 @@
-class Programming_Language:
+class ProgrammingLanguage:
+    obj = 0
     """ Initial variable """
     def __init__(self, language, typing, reflection, year):
         self.language = language
         self.typing = typing
         self.reflection = reflection
         self.year = year
+        self.obj += 1
+
+    """ check whether is language dynamic """
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
+
+    """ print the value """
+    def __str__(self):
+        return "{self.language}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}".format(self=self)
+
+
 
