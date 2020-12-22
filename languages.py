f8f1ccebbc61b74871f04867e31c96e93d25fb97
hoangtuanvp2011 <t00aienl@gmail.com>
2020-12-22 23:31:18 +0700

    create initial variable and import class

---
+++ languages.py
@@ -0,0 +1,8 @@
+from programming_language import Programming_Language
+
+def main():
+    ruby = Programming_Language("Ruby", "Dynamic", True, 1995)
+    python = Programming_Language("Python", "Dynamic", True, 1991)
+    visual_basic = Programming_Language("Visual Basic", "Static", False, 1991)
+if __name__ == '__main__':
+    main()
---
+++ programming_language.py
@@ -0,0 +1,8 @@
+class Programming_Language:
+    """ Initial variable """
+    def __init__(self, language, typing, reflection, year):
+        self.language = language
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
