2ff5e23fb15b234259bce461ca37e6031855c285
hoangtuanvp2011 <t00aienl@gmail.com>
2020-12-23 00:39:57 +0700

    create 3 files doing guitar.py

---
+++ guitar.py
@@ -0,0 +1,20 @@
+class guitars:
+
+    thisyear = 2020
+
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def get_age(self):
+        return self.thisyear - self.year
+
+    def is_vintage(self):
+        if self.get_age() > 50:
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        print("{self.name} {self.year} : {self.cost}".format(self=self))
